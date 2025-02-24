f69b73b2b929d3f62d4b2902b2c9068110eacd20
mhd-ashmal38 <mhdashmal38@gmail.com>
2025-02-24 14:51:29 +0530

    feat: Add mongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from "express"
+import dotenv from "dotenv"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+import connectDB from "./lib/connectDB.js"
+
+dotenv.config();
 
 const app = express()
 
@@ -10,5 +14,6 @@ app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
 
 app.listen(3000, () => {
+    connectDB();
     console.log("Server is running on port 3000")
 })
