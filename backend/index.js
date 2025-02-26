8545f76182a500f7c4db17ad7a5bb6b50aebcf46
mhd-ashmal38 <mhdashmal38@gmail.com>
2025-02-26 15:13:45 +0530

    feat: add clerk users to database

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,12 @@ import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
 import connectDB from "./lib/connectDB.js"
+import webhookRouter from "./routes/webhook.route.js"
 
 dotenv.config();
 
 const app = express()
+app.use("/webhooks", webhookRouter);
 app.use(express.json())
 
 app.use("/users", userRouter);
